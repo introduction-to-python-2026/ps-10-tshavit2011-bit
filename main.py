21d10d1a027affc6544e7a2c3cbb5ed507439ee5
tshavit2011-bit <tshavit2011@gmail.com>
2026-01-13 23:42:05 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,23 +1,33 @@
-import numpy as np
 from image_utils import load_image, edge_detection
 from skimage.filters import median
-from skimage.morphology import ball
+from skimage.morphology import ball, remove_small_objects, opening, disk
 from PIL import Image
+import numpy as np
+import matplotlib.pyplot as plt
 
 
-# load image
-image = load_image("image.png")
+image = load_image("my_image.jpg.jpeg")
 
-# noise suppression
+
 clean_image = median(image, ball(3))
 
-# edge detection
-edgeMAG = edge_detection(clean_image)
 
-# binary conversion (threshold)
-threshold = np.mean(edgeMAG)
-edge_binary = edgeMAG > threshold
+edges = edge_detection(clean_image)
 
-# save result
-edge_image = Image.fromarray(edge_binary.astype(np.uint8) * 255)
+
+edge_binary = edges > 50
+
+
+edge_binary = remove_small_objects(edge_binary, min_size=100)
+
+
+edge_binary = opening(edge_binary, disk(1))
+
+
+plt.imshow(edge_binary, cmap="gray")
+plt.axis("off")
+plt.show()
+
+
+edge_image = Image.fromarray((edge_binary * 255).astype(np.uint8))
 edge_image.save("my_edges.png")
