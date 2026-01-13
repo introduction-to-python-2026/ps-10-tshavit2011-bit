ab6824bc0ec612cde834db3be316220774e07189
tshavit2011-bit <tshavit2011@gmail.com>
2026-01-13 23:18:35 +0200

    Update main.py

--- main.py
+++ main.py
@@ -6,19 +6,14 @@ from skimage import io
 from image_utils import load_image, edge_detection
 
 
-# 1. load original image
-image = load_image("image.png")  # שימי כאן את שם התמונה שלך
+# load image
+image = load_image("image.png")
 
-# 2. remove noise
+# noise suppression
 clean_image = median(image, ball(3))
 
-# 3. edge detection
+# edge detection
 edges = edge_detection(clean_image)
 
-# 4. convert to binary image
-threshold = np.mean(edges)
-binary_edges = edges > threshold
-
-# 5. save result
-io.imsave("edges.png", binary_edges.astype(np.uint8) * 255)
-
+# save result
+io.imsave("edges.png", edges / edges.max())
