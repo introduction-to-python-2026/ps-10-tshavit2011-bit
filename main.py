64f7bc272363af81333c076c75e901da5b5eaab2
tshavit2011-bit <tshavit2011@gmail.com>
2026-01-13 23:22:48 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
 import numpy as np
+from image_utils import load_image, edge_detection
 from skimage.filters import median
 from skimage.morphology import ball
-from skimage import io
-
-from image_utils import load_image, edge_detection
+from PIL import Image
 
 
 # load image
@@ -13,7 +12,12 @@ image = load_image("image.png")
 clean_image = median(image, ball(3))
 
 # edge detection
-edges = edge_detection(clean_image)
+edgeMAG = edge_detection(clean_image)
+
+# binary conversion (threshold)
+threshold = np.mean(edgeMAG)
+edge_binary = edgeMAG > threshold
 
 # save result
-io.imsave("edges.png", edges / edges.max())
+edge_image = Image.fromarray(edge_binary.astype(np.uint8) * 255)
+edge_image.save("my_edges.png")
