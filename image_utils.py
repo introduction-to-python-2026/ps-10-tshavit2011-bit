8a1fa647ef10f9fc6c96833f05da3ee51a3179a8
tshavit2011-bit <tshavit2011@gmail.com>
2026-01-13 23:18:12 +0200

    Update image_utils.py

--- image_utils.py
+++ image_utils.py
@@ -4,32 +4,39 @@ from scipy.signal import convolve2d
 
 
 def load_image(path):
-    # load image using PIL
+    # read image
     image = Image.open(path)
 
     # convert to numpy array
     image = np.array(image)
 
-    # invert colors
-    image = 255 - image
-
     return image
 
 
 def edge_detection(image):
-    # convert to grayscale if image is RGB
+    # convert to grayscale if needed
     if len(image.shape) == 3:
         gray = np.mean(image, axis=2)
     else:
         gray = image
 
-    # Sobel filters
+    # filters (Sobel)
     filter_x = np.array([
         [1, 0, -1],
         [2, 0, -2],
         [1, 0, -1]
     ])
 
-    filter
+    filter_y = np.array([
+        [1,  2,  1],
+        [0,  0,  0],
+        [-1, -2, -1]
+    ])
 
-    
+    edgeX = convolve2d(gray, filter_x, mode='same', boundary='fill', fillvalue=0)
+    edgeY = convolve2d(gray, filter_y, mode='same', boundary='fill', fillvalue=0)
+
+    edgeMAG = edgeX**2 + edgeY**2
+
+    return edgeMAG
+
